5593a9cb5cede34a25e123642960ebaeb7f5f623
Weston Kelly <wkellyuwyo@gmail.com>
2024-04-09 21:29:46 -0700

    car_app views tested tc

--- back-end/car_app/urls.py
+++ back-end/car_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('create/', CreateCar.as_view(), name="create_cars"),
     path('delete/<int:car_id>/', DeleteCar.as_view(), name="delete_car"),
     path('update/<int:car_id>/', UpdateCar.as_view(), name="update_car"),
-    path('review/', ReviewCar.as_view(), name="review_car")
+    path('review/<int:car_id>/', ReviewCar.as_view(), name="review_car")
 ]
